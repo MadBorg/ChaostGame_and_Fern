0f98d24828dcea9cf5acb61933064b19b3c033af
Agathe Raaum <agathebr@mail.uio.no>
2019-11-13 10:46:41 +0100

    Added first try of sierpinski

--- triangle.py
+++ triangle.py
@@ -24,13 +24,32 @@ def triangle(c0,c1,c2=None, plot=False):
         plt.show()
     return c0,c1,c2
 
-def test_triangle():
-    c0,c1,c2 = np.array((0,0)), np.array((0,2)), np.array((1,1))
+
+def ex_b():
+    plt.figure()
+
+    w = np.random.random(3)
+    w /= np.sum(w)
+
+    c = triangle(np.array((0,0)), np.array((1,0)))
+    x = 0
+    for i in range(3):
+        x += w[i]*c[i]
+
+    x_points = np.zeros(1000)
+
+
+    for j in range(5000):
+        i = np.random.randint(3)
+        x_next = (x + c[i])/2
+        plt.plot(x_next[0], x_next[1], ',b')
+
+        x = x_next
+
+    plt.show()
 
 if __name__ == "__main__":
     c0, c1 = np.array((0,0)), np.array((1,0))
     triangle(c0,c1, plot=True)
-        
 
-
-
+    ex_b()
