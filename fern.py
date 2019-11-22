12f16df5e2d77b5ff8303cf44671013e107c9531
Agathe Raaum <agathebr@mail.uio.no>
2019-11-22 15:56:22 +0100

    Changed functions, added iterate and plotting

--- fern.py
+++ fern.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class AffineTransform():
 
     def __init__(self, a=0, b=0, c=0, d=0, e=0, f=0):
@@ -22,43 +23,52 @@ class AffineTransform():
         self.start_point = start_point
 
 
-    def func_1(self, steps=10):
-
-        n_p = np.zeros((steps, 2))
-        n_p[0] = self.start_point
-
-        for i in range(1, steps):
-            n_p[i, 0] = n_p[i-1,0]*0
-            n_p[i, 1] = n_p[i-1,1]*0.16
+    def func_1(self, x):
+        new_x = x[0] * 0
+        new_y = x[1] * 0.16
+        return np.array((new_x, new_y))
 
 
-    def func_2(self, steps=10):
 
-        n_p = np.zeros((steps, 2))
-        n_p[0] = self.start_point
-
-        for i in range(1, steps):
-            n_p[i, 0] = n_p[i-1,0]*0.85 + n_p[i-1,1]*0.04
-            n_p[i, 1] = -n_p[i-1,0]*0.04 + n_p[i-1,1]*0.85 + 1.16
+    def func_2(self, x):
+        new_x = x[0] * 0.85 + x[1] * 0.04
+        new_y = -x[0] * 0.04 + x[1] * 0.85 + 1.16
+        return np.array((new_x, new_y))
 
 
-    def func_3(self, steps=10):
+    def func_3(self, x):
+        new_x = x[0] * 0.2 - x[1] * 0.26
+        new_y = x[0] * 0.23 + x[1] * 0.22 + 1.16
+        return np.array((new_x, new_y))
 
-        n_p = np.zeros((steps, 2))
-        n_p[0] = self.start_point
 
-        for i in range(1, steps):
-            n_p[i, 0] = n_p[i-1,0]*0.2 - n_p[i-1,1]*0.26
-            n_p[i, 1] = n_p[i-1,0]*0.23 + n_p[i-1,1]*0.22 + 1.16
+    def func_4(self, x):
+        new_x = -x[0] * 0.15 + x[1] * 0.28
+        new_y = x[0] * 0.26 + x[1] * 0.24 + 0.44
+        return np.array((new_x, new_y))
 
-    def func_4(self, steps=10):
+    def choose_func(self):
+        p_val = np.array((0.01, 0.85, 0.07, 0.07))
+        p_cumulative = np.cumsum(p_val)
 
-        n_p = np.zeros((steps, 2))
-        n_p[0] = self.start_point
+        functions = [self.func_1, self.func_2, self.func_3, self.func_4]
 
-        for i in range(1, steps):
-            n_p[i, 0] = -n_p[i-1,0]*0.15 + n_p[i-1,1]*0.28
-            n_p[i, 1] = n_p[i-1,0]*0.26 + n_p[i-1,1]*0.24 + 0.44
+        r = np.random.random()
+        for j, p in enumerate(p_cumulative):
+            if r < p:
+                return functions[j]
+
+    def iterate(self):
+        point_list = np.zeros((50000,2))
+        point_list[0] = self.start_point
+
+        for i in range(1, 50000):
+            a = self.choose_func()
+
+            point_list[i] = a(point_list[i-1])
+
+        plt.scatter(*zip(*point_list), marker=".", s=0.2, c='g')
+        plt.show()
 
 
 
@@ -66,7 +76,4 @@ class AffineTransform():
 if __name__ == '__main__':
     test = AffineTransform()
     test.transform(x=1, y=1)
-    test.func_1()
-    test.func_2()
-    test.func_3()
-    test.func_4()
+    test.iterate()
