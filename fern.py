d855459d592a86f9144cbf35ac171f0f46ff9a60
Sander Stokseth Skjulsvik <rednas.1604@gmail.com>
2019-11-24 12:50:02 +0100

    firxed the code, and added dock
    fixed 3 dimentional list problem. and added dock to ferns and choce

--- fern.py
+++ fern.py
@@ -12,103 +12,74 @@ class AffineTransform():
         self.f = f
 
     def __call__(self, x, y):
-        # print(f"\n\n x:{x}, y:{y}\n\n")
         point = np.array((x, y))
         mat = np.array([[self.a, self.b],[self.c, self.d]],np.float)
         ef = np.array((self.e, self.f))
-        # import IPython; IPython.embed()
-        # new_point = (np.cross(point,mat) + ef)
 
-        # print(f"\nmat:{mat}, point:{point}")
-        # print(f"dot:{np.dot(mat,point)}, ef:{ef}\n")
-
-        new_point = np.dot(point, mat) + ef
-
-        # print(f"\nnew_point:{new_point}\n")
+        new_point = np.dot(point.T,mat) + ef
         self.new_point = new_point
         return new_point
-#
-    # def transform(self, x, y):
-    #     point = np.array((x, y))
-    #     mat = np.matrix([[self.a, self.b], [self.c, self.d]])
-    #     ef = np.array((self.e, self.f))
-
-    #     start_point = (np.dot(point, mat) + ef)
-
-    #     self.start_point = start_point
-
-
-    # def func_1(self, steps=10):
-
-    #     n_p = np.zeros((steps, 2))
-    #     n_p[0] = self.start_point
-
-    #     for i in range(1, steps):
-    #         n_p[i, 0] = n_p[i-1,0]*0
-    #         n_p[i, 1] = n_p[i-1,1]*0.16
-
-
-    # def func_2(self, steps=10):
-
-    #     n_p = np.zeros((steps, 2))
-    #     n_p[0] = self.start_point
-
-    #     for i in range(1, steps):
-    #         n_p[i, 0] = n_p[i-1,0]*0.85 + n_p[i-1,1]*0.04
-    #         n_p[i, 1] = -n_p[i-1,0]*0.04 + n_p[i-1,1]*0.85 + 1.16
-
-
-    # def func_3(self, steps=10):
-
-    #     n_p = np.zeros((steps, 2))
-    #     n_p[0] = self.start_point
-
-    #     for i in range(1, steps):
-    #         n_p[i, 0] = n_p[i-1,0]*0.2 - n_p[i-1,1]*0.26
-    #         n_p[i, 1] = n_p[i-1,0]*0.23 + n_p[i-1,1]*0.22 + 1.16
-
-    # def func_4(self, steps=10):
-
-    #     n_p = np.zeros((steps, 2))
-    #     n_p[0] = self.start_point
-
-    #     for i in range(1, steps):
-    #         n_p[i, 0] = -n_p[i-1,0]*0.15 + n_p[i-1,1]*0.28
-    #         n_p[i, 1] = n_p[i-1,0]*0.26 + n_p[i-1,1]*0.24 + 0.44
 
  
-def cumChoice(p, n):
+def cumChoice(d, n):
+    """
+    Params:
+    ----
+    d np.array(mxn)<float>
+        Distribution for the cumulative distributions
+    n int
+        number for choices
+    Returns:
+    ----
+    choices
+        n Choises derrived from the distribution
+    """
     r = np.random.random(n)
-    p_cumulative = np.cumsum(p)
+    d_cumulative = np.cumsum(d)
     choices = np.zeros(n, dtype="int")
 
     for i in range(n):
-        for j, p in enumerate(p_cumulative):
-            if r[i] < p:
+        for j, d in enumerate(d_cumulative):
+            if r[i] < d:
                 choices[i] = j
                 break
     return choices
 
-
 def ferns(parameters, distribution, n, x0=0):
+    """
+    Params:
+    ----
+    parameters: (m * k) iterable float
+        Parameters for the different fern instances
+    distribution: (m) iterable float
+        The distribution of where the fern instances is going to be picked.
+        sum(distribution) == 1
+    x0: (2) iterable
+        starting point
+    Returns:
+    ----
+    x (n,2) np.array
+        coordinate points
+
+    """
+    eps = 1e-13
+    assert sum(distribution) - 1 < eps, f"sum(distribution must be 1) it is {sum(distribution)}"
+    assert len(parameters) == len(distribution), f"lenth of parameters must be equal to lenght of distribution: len(parameters):{len(parameters)}, len(distribution):{len(distribution)}"
+    # import IPython; IPython.embed()
+
     f = []
     for i in parameters:
-        f.append(AffineTransform(*i))
+        print(f"\n i:{i}")
+        f.append(AffineTransform(a=i[0], b=i[1],c=i[2],d=i[3],e=i[4],f=i[5]))
     x = np.zeros((n,2))
     x[0] = x0
     choices = cumChoice(distribution, n)
     for i in range(0, len(choices)):
-        # print(f"\nx[i-1]:{x[i-1]}\n")
-        x[i] = f[choices[i]](x[i-1][0],x[i-1][1])
-        # print(x[i-1])
-        # x[i] = f[choices[i]](*zip(x[i-1]))
 
+        x[i] = f[choices[i]](*zip(x[i-1]))
     return x
 
     
-
-
-
 if __name__ == '__main__':
     parameters= (
         (    0,    0,    0, 0.16,   0,    0),
@@ -119,7 +90,7 @@ if __name__ == '__main__':
     distribution=(0.01, 0.85, 0.07, 0.07)
     n = 50_000
     points = ferns(parameters, distribution, n)
-    plt.scatter(*zip(*points), marker=".", s=0.2, alpha=0.2)
+    plt.scatter(*zip(*points), marker=".", s=0.2, alpha=0.2, c="g")
     plt.show()
 
     # test = AffineTransform()
