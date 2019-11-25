5d8330846bffdc5ecf204580104692c979a7a72d
Sander Stokseth Skjulsvik <rednas.1604@gmail.com>
2019-11-25 13:17:40 +0100

    added variations, with class Variations
    Made init with some asserts as dtype changes if needed.

---
+++ variations.py
@@ -0,0 +1,30 @@
+import numpy as np
+# import matplotlib.pyplot as plt
+
+class Variations:
+
+    def __init__(self, x,y, colors="black"):
+       #dock
+       #asserts
+        try:
+            iter(x)
+        except:
+            raise TypeError("x is not an iterable")
+        try:
+            iter(y)
+        except:
+            raise TypeError("y is not an iterable")
+            
+      #selfs
+       #change dtype
+        if type(x) != np.ndarray:
+            self.x = np.array(x, dtype="float_")
+        else:
+            self.x = x
+        if type(y) != np.ndarray:
+            self.y = np.array(y, dtype="float_") 
+        else:
+            self.y = y
+       #standard
+        self.colors = colors
+         
