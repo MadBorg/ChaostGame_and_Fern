b373a004421860aceea4962f865628c6dfa3bbc9
Sander Stokseth Skjulsvik <rednas.1604@gmail.com>
2019-11-25 14:52:38 +0100

    properties, and mapped out variations
    properties:
      r, theta, phi
    Variations:
      linear, handkercheif, swirl, disc

--- variations.py
+++ variations.py
@@ -1,11 +1,17 @@
 import numpy as np
 # import matplotlib.pyplot as plt
 
-class Variations:
 
-    def __init__(self, x,y, colors="black"):
-       #dock
-       #asserts
+class Variations:
+    """
+    Altrering coordinates.
+    So if you have a set of coordinates it alters them in a specific way..
+    """
+
+    def __init__(self, x, y, colors="black"):
+       # dock
+
+       # asserts
         try:
             iter(x)
         except:
@@ -14,17 +20,55 @@ class Variations:
             iter(y)
         except:
             raise TypeError("y is not an iterable")
-            
-      #selfs
-       #change dtype
+
+      # selfs
+       # change dtype
         if type(x) != np.ndarray:
             self.x = np.array(x, dtype="float_")
         else:
             self.x = x
         if type(y) != np.ndarray:
-            self.y = np.array(y, dtype="float_") 
+            self.y = np.array(y, dtype="float_")
         else:
             self.y = y
-       #standard
+
+       # standard
         self.colors = colors
-         
+
+       # private
+        self._n = len(x)
+        self._u = x
+        self._v = y
+   # Properties
+    @property
+    def r(self):
+        return np.sqrt(self.x**2 + self.y**2)
+
+    @property
+    def theta(self):
+        return np.arctan2(self.x, self.y)
+
+    @property
+    def phi(self):
+        return np.arctan2(self.y, self.x)
+
+   # Variations
+    def linear():
+        self._u = self._u
+        self._v = self._v
+
+    def handkerchief():
+        self._u = r * sin()
+
+    def swirl():
+        pass
+
+    def disc():
+        pass
+
+    # choose atleast two more to implement.
+    def fun1():
+        pass
+
+    def fun2():
+        pass
