862d437505379c6bed06e216d8be420274fe666d
Sander Stokseth Skjulsvik <rednas.1604@gmail.com>
2019-11-25 15:11:33 +0100

    fixed bug in np.all (all of them), and test_r

--- test_variations.py
+++ test_variations.py
@@ -25,6 +25,17 @@ def test_variatons_init_dtypes():
 # def test_variatons_init_color():
 #     #test if color exist
 
+def test_variatons_r():
+    x = (0, 0, 1, 2,          1,                   4)
+    y = (0, 1, 0, 0,          1,                   3)
+    r = (0, 1, 1, 2, np.sqrt(2), np.sqrt(4**2 + 3**2))
+    # r = (0, 0, 0, 0,          0,                   0)
+    x,y,r = np.array(x), np.array(y), np.array(r)
+    test = variations.Variations(x, y)
+    eps = 1e-13
+    # import IPython; IPython.embed()
+    assert np.all(abs(test.r - r) < eps), "test_variatons_r failed over: all(abs(test.r - r)) < eps!"
+
 def test_variatons_linear():
     n = 1e5
     x = np.linspace(-10, 100, n)*3
@@ -32,10 +43,14 @@ def test_variatons_linear():
     test = variations.Variations(x, y)
     test.linear()
     eps = 1e-14
-    assert all(test._u - x) < eps, "linear changed the x values"
-    assert all(test._v - y) < eps, "linear changed the y values"
+    # test._u *= 10
+    # test._v *= 10
+    assert np.all(abs(test._u - x) < eps), "linear changed the x values"
+    assert np.all(abs(test._v - y) < eps), "linear changed the y values"
 
 
 if __name__ == "__main__":
     test_variatons_init_coordinates_shape()
     test_variatons_init_dtypes()
+    test_variatons_linear()
+    test_variatons_r()
