693ef3d178bc663d4ed6e82d4842aa6fd5fd3ba8
Agathe Raaum <agathebr@mail.uio.no>
2019-11-26 09:10:03 +0100

    small format changes

--- fern.py
+++ fern.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class AffineTransform():
 
+class AffineTransform:
     def __init__(self, a=0, b=0, c=0, d=0, e=0, f=0):
         self.a = a
         self.b = b
@@ -13,10 +13,10 @@ class AffineTransform():
 
     def __call__(self, x, y):
         point = np.array((x, y))
-        mat = np.array([[self.a, self.b],[self.c, self.d]],np.float)
+        mat = np.array([[self.a, self.b], [self.c, self.d]], np.float)
         ef = np.array((self.e, self.f))
 
-        new_point = np.dot(mat,point) + ef
+        new_point = np.dot(mat, point) + ef
         self.new_point = new_point
         return new_point
 
@@ -45,6 +45,7 @@ def cumChoice(d, n):
                 break
     return choices
 
+
 def ferns(parameters, distribution, n, x0=0):
     """
     Params:
@@ -63,35 +64,42 @@ def ferns(parameters, distribution, n, x0=0):
 
     """
     eps = 1e-13
-    assert sum(distribution) - 1 < eps, f"sum(distribution must be 1) it is {sum(distribution)}"
-    assert len(parameters) == len(distribution), f"lenth of parameters must be equal to lenght of distribution: len(parameters):{len(parameters)}, len(distribution):{len(distribution)}"
+    assert (
+        sum(distribution) - 1 < eps
+    ), f"sum(distribution must be 1) it is {sum(distribution)}"
+    assert len(parameters) == len(
+        distribution
+    ), f"lenth of parameters must be equal to lenght of distribution: len(parameters):{len(parameters)}, len(distribution):{len(distribution)}"
     # import IPython; IPython.embed()
 
     f = []
     for i in parameters:
         # print(f"\n i:{i}")
-        f.append(AffineTransform(a=i[0], b=i[1],c=i[2],d=i[3],e=i[4],f=i[5]))
-    x = np.zeros((n,2))
+        f.append(AffineTransform(a=i[0], b=i[1], c=i[2], d=i[3], e=i[4], f=i[5]))
+    x = np.zeros((n, 2))
     x[0] = x0
     choices = cumChoice(distribution, n)
     for i in range(0, len(choices)):
         # print(f"x[i]:{x[i]}, *zip(z[i]):{x[i]))}")
-        x[i] = f[choices[i]](x[i-1, 0], x[i-1, 1])
+        x[i] = f[choices[i]](x[i - 1, 0], x[i - 1, 1])
     return x
 
-def fun(x,y):
-    return (x,y)
-if __name__ == '__main__':
-    parameters= (
-        (    0,    0,    0, 0.16,   0,    0),
-        ( 0.85, 0.04,-0.04, 0.85,   0, 1.60),
-        ( 0.20,-0.26, 0.23, 0.22,   0, 1.60),
-        (-0.15,0.28 , 0.26, 0.24,   0, 0.44)
+
+def fun(x, y):
+    return (x, y)
+
+
+if __name__ == "__main__":
+    parameters = (
+        (0, 0, 0, 0.16, 0, 0),
+        (0.85, 0.04, -0.04, 0.85, 0, 1.60),
+        (0.20, -0.26, 0.23, 0.22, 0, 1.60),
+        (-0.15, 0.28, 0.26, 0.24, 0, 0.44),
     )
-    distribution=(0.01, 0.85, 0.07, 0.07)
+    distribution = (0.01, 0.85, 0.07, 0.07)
     n = 50_000
     points = ferns(parameters, distribution, n)
     plt.scatter(*zip(*points), marker=".", s=0.2, alpha=0.2, c="g")
-    plt.axis('equal')
-    plt.axis('off')
+    plt.axis("equal")
+    plt.axis("off")
     plt.show()
