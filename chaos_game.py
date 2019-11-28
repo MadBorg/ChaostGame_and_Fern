e4e6435ba614edb7ccf5e03b24d42be43487f8ff
Agathe Raaum <agathebr@mail.uio.no>
2019-11-29 00:41:49 +0100

    added comments, not finished

--- chaos_game.py
+++ chaos_game.py
@@ -10,10 +10,10 @@ class ChaosGame:
     n : int, default: 3
         Number of sides in n-gon.
     r : float, default: 1/2
-        Ratio between two points.
+        Ratio between two points on.
     """
 
-    def __init__(self, n=3, r=1/2):
+    def __init__(self, n=3, r=1 / 2):
         self.n = n
         self.r = r
 
@@ -25,9 +25,9 @@ class ChaosGame:
             raise ValueError(f"n must be larger than one. n:{n}")
 
         self._generate_ngon()
-        self._starting_point()
 
     def _generate_ngon(self):
+        """Generates corners of n-gon."""
         n = self.n
         theta = np.linspace(0, 2 * np.pi, n + 1)
         c = np.zeros((n, 2))
@@ -38,83 +38,113 @@ class ChaosGame:
         self.c = c
 
     def plot_ngon(self):
+        """Plots corners of n-gon."""
         plt.scatter(*zip(*self.c))
         plt.axis("equal")
         plt.show()
 
     def _starting_point(self):
+        """Generates a starting point inside n-gon."""
         n = self.n
         w = np.random.random(n)
         w /= np.sum(w)
-        x = np.dot(w, self.c)
 
-        self.x = x
+        return np.dot(w, self.c)
 
-    def iterate(self, steps=100000, discard=5):
+    def iterate(self, steps, discard=5):
+        """Generates n points in n-gon.
+
+        Parameters
+        -----------
+        steps : int
+            Number of (x,y) points to be generated.
+        discard : int, default: 5
+            Number of first points from starting point to be discarded.
+
+        Notes
+        ------
+        This method returns nothing, but stores but stores (x,y) points
+        and corner corresponding points internally in the class.
+        """
         r = self.r
         c = self.c
-        x = self.x
         k = np.random.randint(self.n, size=steps)
 
-        y = np.zeros((steps, 2))
-        y[0] = x
+        x = np.zeros((steps, 2))
+        x[0] = self._starting_point()
 
         for i in range(1, steps):
-            y[i] = (r * y[i - 1]) + ((1 - r) * c[k[i]])
-        self.y, self.k = y[discard:], k[discard:]
+            x[i] = (r * x[i - 1]) + ((1 - r) * c[k[i]])
+        self.x, self.k = x[discard:], k[discard:]
 
     @property
-    def x_points(self):
-        return self.y[:, 0]
+    def x_values(self):
+        try:
+            return self.x[:, 0]
+        except AttributeError:
+            raise AttributeError("Iterate method must be called to get x-values.")
 
     @property
-    def y_points(self):
-        return self.y[:, 1]
+    def y_values(self):
+        try:
+            return self.x[:, 1]
+        except AttributeError:
+            raise AttributeError("Iterate method must be called to get y-values.")
+
+    @property
+    def color(self):
+        try:
+            return self.col
+        except AttributeError:
+            return self._compute_color(self.k)
 
     def plot(self, color=False, cmap="jet"):
+        """Plot for chaos game.
+
+        Parameters
+        ----------
+        color : bool, default: False
+            XXXXXXXXXXXXXXXXXXXXXXXXXX
+        cmap : str, default: "jet"
+            Matplotlib colormap.
+        """
         try:
-            y, k = self.y, self.k
+            x, k = self.x, self.k
         except AttributeError:
-            raise AttributeError()
+            raise AttributeError("Iterate method must be called.")
+
         fig, ax = plt.subplots()
         if color:
-            ax.scatter(*zip(*y), marker=".", s=0.2, cmap="jet", c=k)
-        elif color is None:
             g = self._compute_color(k)
-            print(
-                f"\n\ng.size:{g.shape}, y.shape:{y.shape}, k.shape:{k.shape}\n k:{k}\n\n "
-            )
-            ax.scatter(*zip(*y), marker=".", s=0.2, cmap="jet", c=g)
+            ax.scatter(*zip(*x), marker=".", s=0.2, cmap=cmap, c=g)
         else:
-            ax.scatter(*zip(*y), marker=".", s=0.2)
+            ax.scatter(*zip(*x), marker=".", s=0.2, c="black")
 
-        ax.axis("equal")
-        # return fig, ax
+        plt.axis("equal")
+        plt.axis("off")
+        self.fig = fig
+
+    def _compute_color(self, k):
+        """Generates list of gradient color values corresponding to x points"""
+        col = np.empty(len(k))
+        col[0] = k[0]
+        for i in range(1, len(k)):
+            col[i] = (col[i - 1] + k[i]) / 2
+
+        return col
 
     def show(self, color=False, cmap="jet"):
-        # fig, ax = self.plot(color, cmap)
         self.plot(color, cmap)
         plt.show()
 
-    def _compute_color(self, k):
-        len_k = len(k)  # number of iterations
-        n = self.n  # vertices
-
-        col = np.empty(len_k)
-        col[0] = k[0]
-        for i in range(1, len_k):
-            col[i] = (col[i - 1] + k[i])/2
-        # col = col / self.n
-
-        return col
+    #NOT FINISHED
+    def savepng(self, outfile, color=False, cmap="jet"):
+        self.show(color, cmap)
+        self.fig.savefig(outfile)
 
 
 if __name__ == "__main__":
 
-    # for i in range(3, 9):
-    # test = ChaosGame(i)
-    # test.plot_ngon()
-
-    test = ChaosGame(n=4, r=1/2)
-    test.iterate()
-    test.show(color=None)
+    test = ChaosGame(n=6, r=1/3)
+    test.iterate(steps=100_000)
+    test.show(color=True, cmap="twilight")
